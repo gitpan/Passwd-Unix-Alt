8fb10ea6611b769f939bf7594161fe988f7988ff
Steven Haryanto <stevenharyanto@gmail.com>
2011-04-22 12:35:01 +0000

    Import of SHARYANTO/Passwd-Unix-Alt-0.5205 from CPAN.
    gitpan-cpan-distribution: Passwd-Unix-Alt
    gitpan-cpan-version:      0.5205
    gitpan-cpan-path:         SHARYANTO/Passwd-Unix-Alt-0.5205.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Passwd/Unix/Alt.pm
+++ lib/Passwd/Unix/Alt.pm
@@ -48,7 +48,7 @@ my $_CHECK = {
 	'uid'		=> sub { return if not defined $_[0] or $_[0] !~ /^[0-9]+$/o; TRUE },
 	'home'		=> sub { return if not defined $_[0] or $_[0] !~ PATH; TRUE },
 	'shell'		=> sub { return if not defined $_[0] or $_[0] !~ PATH; TRUE },
-	'gecos'		=> sub { return if not defined $_[0] or $_[0] !~ /^[^:]*$/o; TRUE },
+	'gecos'		=> sub { return if not defined $_[0] or $_[0] !~ /^[^:\r\n]*$/o; TRUE },
 	'passwd' 	=> sub { return if not defined $_[0]; TRUE},
 };
 #======================================================================
@@ -882,7 +882,7 @@ Passwd::Unix::Alt - Manipulate /etc/{passwd,shadow,group,gshadow} entries
 
 =head1 VERSION
 
-version 0.5204
+version 0.5205
 
 =head1 SYNOPSIS
 
